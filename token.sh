4877c02f8b16317333bedb9341d79e66b50284bc
Luwei <lw1023@msn.com>
2020-03-01 15:22:56 +0800

    优化token获取脚本，将直接从微信服务器获取改为从本地Token中控服务器获取

--- token.sh
+++ token.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-# 配置信息
-appId="wx******"
-secret="*****"
+# 获取Token的URL
+url="http://localhost:9010/token"
 
-url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appId&secret=$secret"
+info=$(curl $url)
 
-function httpRequest() {
-  #curl 请求
-  info=$(curl $url)
-
-  #获取返回码
-  token=$(echo $info | jq -r '.access_token')
-
-  echo $token >token
-
-  #对响应码进行判断
-  # if [ "$code" == "200" ]; then
-  #   echo "请求成功，响应码是$code"
-  # else
-  #   echo "请求失败，响应码是$code"
-  # fi
-}
-
-httpRequest
+#获取返回码
+echo $info | jq -r '.token'
